f3e5754cc092ad14ccdaace3098681b0dcd7bc56
Visivel <120154724+Visivel@users.noreply.github.com>
2025-11-28 15:23:26 -0300

    DO NOT REDEEM IT
    do not delete my entire disko :sob:

--- hosts/laptop/system/disko.nix
+++ hosts/laptop/system/disko.nix
@@ -3,90 +3,24 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/sdX";
+        device = "/dev/nvme0n1";
         content = {
-          type = "gpt";
           partitions = {
-            ESP = {
-              size = "1G";
-              type = "EF00";
+            linux = {
+              type = "partition";
+              device = "/dev/nvme0n1p9";
               content = {
                 type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" "fmask=0077" "dmask=0077" ];
-              };
-            };
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted";
-                extraOpenArgs = [ 
-                  "--allow-discards" 
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue"
-                ];
-                settings = {
-                  allowDiscards = true;
-                  bypassWorkqueues = true;
+                format = "btrfs";
+                mountpoint = "/";
+                subvolumes = {
+                  "/root" = { mountpoint = "/"; };
+                  "/nix" = { mountpoint = "/nix"; };
+                  "/home" = { mountpoint = "/home"; };
+                  "/var" = { mountpoint = "/var"; };
+                  "/var/log" = { mountpoint = "/var/log"; };
                 };
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" "--csum xxhash" ];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [ 
-                        "compress=zstd:1" 
-                        "noatime" 
-                        "space_cache=v2" 
-                        "discard=async"
-                        "ssd"
-                      ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ 
-                        "compress=zstd:1" 
-                        "noatime" 
-                        "space_cache=v2" 
-                        "discard=async"
-                        "ssd"
-                      ];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = [ 
-                        "compress=zstd:3" 
-                        "noatime" 
-                        "space_cache=v2" 
-                        "discard=async"
-                        "ssd"
-                      ];
-                    };
-                    "/var" = {
-                      mountpoint = "/var";
-                      mountOptions = [ 
-                        "compress=zstd:1" 
-                        "noatime" 
-                        "space_cache=v2" 
-                        "discard=async"
-                        "ssd"
-                      ];
-                    };
-                    "/var/log" = {
-                      mountpoint = "/var/log";
-                      mountOptions = [ 
-                        "compress=zstd:1" 
-                        "noatime" 
-                        "space_cache=v2" 
-                        "discard=async"
-                        "ssd"
-                      ];
-                    };
-                  };
-                };
+                mountOptions = [ "compress=zstd:1" "noatime" "space_cache=v2" "discard=async" "ssd" ];
               };
             };
           };
